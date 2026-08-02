999ab62da6b837ed38b99a1bb405f71bc3da92c7
Tran Linh <stebenc09122002@gmail.com>
2026-08-02 14:55:10 +0000

    [hvac-service] Init HVAC Manager/Service/AIDL (Comfort domain) [issue tracker] N/A [description] Add service/comfort/hvac/: HvacManager/HvacService extending the Comfort base classes, AIDL interfaces, and helper classes for the HVAC property set. [symptom] N/A [rootcause] N/A [action] Added Android.bp (split hvac-manager-sdk + hvac-service), AndroidManifest.xml (BIND_HVAC_SERVICE, sharedUserId="android.uid.system", persistent="true"), aidl/{HvacEvent, IHVACVehicleCallback,IHVACVehicleService}, and src/ (HvacEvent, HvacListener, HvacManager, HvacProperties, IHvacController, SystemListener, HvacService). HvacManager resolves the service via ServiceManager.getService("hvac_service"); HvacService publishes itself with ServiceManager.addService(...) in onCreate(). [test step] Full product build (m); flashed emulator, toggled AC/temperature/seat heater/ventilation via hvac_app and confirmed onChangeEvent callbacks reach the ViewModel. [test rate] pass

---
+++ kpit/automotive/service/comfort/hvac/src/com/kpit/hvac/manager/HvacListener.java
@@ -0,0 +1,23 @@
+package com.kpit.hvac.manager;
+
+public interface HvacListener {
+    void onFanSpeedChanged(int speed);
+
+    void onACStateChanged(boolean value);
+
+    void onMaxStateChanged(boolean value);
+
+    void onAirRecycleStateChanged(boolean value);
+
+    void onTempChanged(int value, int area);
+
+    void onSyncStateChanged(boolean value);
+
+    void onHeatingSeatChanged(boolean value, int area);
+
+    void onVentilationModeChanged(int value);
+
+    void onAutoStateChanged(boolean value);
+
+    void onDefrostStateChanged(boolean value);
+}
